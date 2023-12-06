e9c4458e8d91a6904bc613cbf19423c7044f2765
Dimitri Sophinos <dpsophinos@gmail.com>
2023-12-05 23:54:48 -0500

    add authenticity certification

--- ChipConfig.h
+++ ChipConfig.h
@@ -1,3 +1,25 @@
+/*
+Author        : Dimitri Sophinos
+Class         : CSI - 240 : Advanced Programming
+Assignment    : Final project
+Due Date      : 12/5/23 11:59PM
+*/
+/*
+Project Description :
+Create an interpreter for the CHIP-8 system.
+
+Certification of Authenticity :
+
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the
+definition and consequences of plagiarism and acknowledge that the assessor
+of this assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking
+service (which may then retain a copy of this assignment on its
+database for the purpose of future plagiarism checking)
+*/
 #pragma once
 
 
